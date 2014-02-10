29b6e48be45484e8b5a3c790c697e50fcdfdc0af
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-02-10 15:18:51 +0100

    [add] tests for two chunks

--- connector_file/tests/test_pure.py
+++ connector_file/tests/test_pure.py
@@ -34,6 +34,9 @@ class TestParsePolicy(unittest2.TestCase):
         super(TestParsePolicy, self).setUp()
         env = Mock()
         self.parse_policy = ParsePolicy(env)
+        self.expected_parsed_header = '["ref", "date", "period_id", "journal_id", "line_id/account_id", "line_id/partner_id", "line_id/name", "line_id/analytic_account_id", "line_id/debit", "line_id/credit", "line_id/tax_code_id"]'  # noqa
+        self.expected_parsed_chunk = '[["1728274", "2014-02-02", "02\\\\/2014", "Sales Journal - (test)", "X11001", "Bank", "Camptocamp", "", "37.8", "", ""], ["", "", "", "", "X1111", "Bank", "Camptocamp", "AA009", "", "31.5", "taxcode1"], ["", "", "", "", "X2001", "Bank", "Camptocamp", "AA001", "", "3.83", "taxcode1"], ["", "", "", "", "X2110", "Bank", "Camptocamp", "AA001", "3.83", "", "taxcode1"], ["", "", "", "", "X1000", "Bank", "Camptocamp", "", "", "6.3", "taxcode2"], ["", "", "", "", "X1000", "Bank", "Camptocamp", "", "", "-0", "taxcode2"]]'  # noqa
+        self.expected_parsed_chunk_2 = '[["1728277", "2014-02-02", "02\\\\/2014", "Sales Journal - (test)", "X11001", "Bank", "OpenERP SA", "", "56.4", "", ""], ["", "", "", "", "X1111", "Bank", "OpenERP SA", "AA009", "", "43.17", "taxcode1"], ["", "", "", "", "X2001", "Bank", "OpenERP SA", "AA001", "", "3.83", "taxcode1"], ["", "", "", "", "X1000", "Bank", "OpenERP SA", "", "", "8.63", "taxcode2"], ["", "", "", "", "X1000", "Bank", "OpenERP SA", "", "", "0.77", "taxcode2"]]'  # noqa
 
 
 class TestSplitDataInChunks(TestParsePolicy):
@@ -86,7 +89,44 @@ class TestSplitDataInChunks(TestParsePolicy):
 
         self.assertEquals(2, result_chunk['line_start'])
         self.assertEquals(8, result_chunk['line_stop'])
-        self.assertEquals('[["1728274", "2014-02-02", "02\\\\/2014", "Sales Journal - (test)", "X11001", "Bank", "Camptocamp", "", "37.8", "", ""], ["", "", "", "", "X1111", "Bank", "Camptocamp", "AA009", "", "31.5", "taxcode1"], ["", "", "", "", "X2001", "Bank", "Camptocamp", "AA001", "", "3.83", "taxcode1"], ["", "", "", "", "X2110", "Bank", "Camptocamp", "AA001", "3.83", "", "taxcode1"], ["", "", "", "", "X1000", "Bank", "Camptocamp", "", "", "6.3", "taxcode2"], ["", "", "", "", "X1000", "Bank", "Camptocamp", "", "", "-0", "taxcode2"]]', result_chunk['prepared_data'])
+        self.assertEquals(
+            self.expected_parsed_chunk,
+            result_chunk['prepared_data']
+        )
+
+    def test_two_chunks(self):
+        """It should return two chunks when given such a CSV."""
+
+        input_file = open(self._expand_path('two_chunks.csv'))
+
+        result = self.parse_policy._split_data_in_chunks(input_file)
+        result_list = list(result)
+
+        self.assertEquals(2, len(result_list))
+
+        # first chunk
+        self.assertItemsEqual([
+            'line_start', 'line_stop', 'prepared_data'
+        ], result_list[0].keys())
+
+        self.assertEquals(2, result_list[0]['line_start'])
+        self.assertEquals(8, result_list[0]['line_stop'])
+        self.assertEquals(
+            self.expected_parsed_chunk,
+            result_list[0]['prepared_data']
+        )
+
+        # second chunk
+        self.assertItemsEqual([
+            'line_start', 'line_stop', 'prepared_data'
+        ], result_list[1].keys())
+
+        self.assertEquals(8, result_list[1]['line_start'])
+        self.assertEquals(13, result_list[1]['line_stop'])
+        self.assertEquals(
+            self.expected_parsed_chunk_2,
+            result_list[1]['prepared_data']
+        )
 
 
 class TestParseHeaderData(TestParsePolicy):
@@ -109,7 +149,7 @@ class TestParseHeaderData(TestParsePolicy):
 
         result = self.parse_policy._parse_header_data(input_file)
 
-        self.assertEquals(result, '["ref", "date", "period_id", "journal_id", "line_id/account_id", "line_id/partner_id", "line_id/name", "line_id/analytic_account_id", "line_id/debit", "line_id/credit", "line_id/tax_code_id"]')
+        self.assertEquals(result, self.expected_parsed_header)
 
     def test_one_chunk(self):
         """It should return the header when given one chunk."""
@@ -118,4 +158,4 @@ class TestParseHeaderData(TestParsePolicy):
 
         result = self.parse_policy._parse_header_data(input_file)
 
-        self.assertEquals(result, '["ref", "date", "period_id", "journal_id", "line_id/account_id", "line_id/partner_id", "line_id/name", "line_id/analytic_account_id", "line_id/debit", "line_id/credit", "line_id/tax_code_id"]')
+        self.assertEquals(result, self.expected_parsed_header)
